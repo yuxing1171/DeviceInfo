3bdee7f7a12dbc4d6e1d49cfce3c0b769eae91d4
yuxing <yuxinghappy@gmail.com>
2013-11-01 15:45:28 +0800

    add some

--- src/me/yuxing/deviceinfo/MainActivity.java
+++ src/me/yuxing/deviceinfo/MainActivity.java
@@ -1,7 +1,11 @@
 package me.yuxing.deviceinfo;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.LayoutInflater;
@@ -30,6 +34,9 @@ public class MainActivity extends Activity {
         addTableRow(R.string.density_dpi, outMetrics.densityDpi + " ppi");
         addTableRow(R.string.screen_width_dip, Math.round(outMetrics.widthPixels/outMetrics.density) + " dp");
         addTableRow(R.string.screen_height_dip, Math.round(outMetrics.heightPixels/outMetrics.density) + " dp");
+        addTableRow(R.string.android_id, android.provider.Settings.Secure.getString(getContentResolver(), android.provider.Settings.Secure.ANDROID_ID));
+        addTableRow(R.string.imei, getImei(this));
+        addTableRow(R.string.mac_address, getMacAddress(this));
     }
     
     private void addTableRow(int label, String value) {
@@ -47,5 +54,16 @@ public class MainActivity extends Activity {
     
     private void addTableRow(int label, float value) {
         addTableRow(label, String.valueOf(value));
+    }
+    
+    public static String getImei(Context context) {
+    	TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+    	return tm.getDeviceId();
+    }
+    
+    public static String getMacAddress(Context context) {
+    	WifiManager wifi = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+    	WifiInfo wifiInfo = wifi.getConnectionInfo();
+    	return wifiInfo.getMacAddress();
     }
 }
